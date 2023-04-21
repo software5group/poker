198f2f4f20f8ee2de5f2bb14a4403be5d0e8477f
makajun <121440026+makajun@users.noreply.github.com>
2023-04-21 21:02:10 +0900

    Add files via upload

---
+++ PokerRules.java
@@ -0,0 +1,10 @@
+public class PokerRules {
+
+    // ポーカーのルールを表示するメソッド
+    public static String showRules() {
+        String rules = "ポーカーのルールを説明します。\n"
+        + "まずプレイヤーは5枚のカードを引きます。\n"
+        + "カードを受け取ったら";
+        return rules;
+    }
+}
